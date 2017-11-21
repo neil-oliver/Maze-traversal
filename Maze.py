cc1636acd4fc604d4f02f214b2b6a7634109ac4c
neil-oliver <neiloliverphoto@gmail.com>
2017-11-21 10:03:32 +0000

    grid based location changes
    changes the names of the nodes to grid locations and calculated the straight line distance.

--- Maze.py
+++ Maze.py
@@ -1,100 +1,102 @@
 import operator
+import string
+from math import sqrt
 
 #start and end variables
-start = "A"
-end = "BO"
+start = "A15"
+end = "P1"
 
 #graph representation of https://i.redd.it/qzmfijn9kewz.gif
 graph = {
-    "A": [{"B": 8}, {"DA": 11}],
-    "DA": [{"AK": 30}, {"AL": 1}],
-    "B": [{"C": 14}, {"D": 1}],
-    "C": [{"G": 8}, {"H": 3}],
-    "D": [{"E": 1}, {"F": 5}],
-    "E": [],
-    "F": [],
-    "G": [{"I": 5}, {"J": 6}],
-    "H": [],
-    "I": [{"M": 3}, {"N": 11}],
-    "J": [{"K": 2}, {"L": 3}],
-    "K": [],
-    "L": [],
-    "M": [],
-    "N": [{"O": 15}, {"P": 9}],
-    "O": [{"U": 1}, {"V": 9}],
-    "P": [{"Q": 6}, {"R": 7}],
-    "Q": [{"AI": 1}, {"AJ": 2}],
-    "R": [{"T": 5}, {"S": 2}],
-    "S": [],
-    "T": [],
-    "U": [],
-    "V": [{"W": 1}, {"X": 5}],
-    "W": [],
-    "X": [{"Y": 7}, {"Z": 9}],
-    "Y": [{"AA": 1}, {"AB": 4}],
-    "Z": [{"AE": 4}, {"AF": 37}],
-    "AA": [{"AC": 2}, {"AD": 1}],
-    "AB": [],
-    "AC": [],
-    "AD": [],
-    "AE": [{"AG": 1}, {"AH": 1}],
-    "AF": [],
-    "AG": [],
-    "AH": [],
-    "AI": [],
-    "AJ": [],
-    "AK": [{"AM": 2}, {"AN": 5}],
-    "AL": [],
-    "AM": [],
-    "AN": [{"DB": 1}, {"DC": 5}],
-    "DB": [],
-    "DC": [{"AP": 7}, {"AO": 4}],
-    "AO": [{"BA": 9}, {"BB": 2}],
-    "AP": [{"AQ": 1}, {"AR": 2}],
-    "AQ": [],
-    "AR": [{"AS": 1}, {"AT": 3}],
-    "AS": [],
-    "AT": [{"AU": 1}, {"AV": 2}],
-    "AU": [],
-    "AV": [{"AW": 4}, {"AX": 3}],
-    "AW": [],
-    "AX": [{"AY": 6}, {"AZ": 4}],
-    "AY": [],
-    "AZ": [],
-    "BA": [{"BE": 4}, {"BF": 5}],
-    "BB": [{"BD": 2}, {"BC": 1}],
-    "BC": [],
-    "BD": [],
-    "BE": [{"BG": 1}, {"BH": 1}],
-    "BF": [{"BI": 2}, {"BJ": 1}],
-    "BG": [],
-    "BH": [],
-    "BI": [{"BK": 3}, {"BL": 2}],
-    "BJ": [],
-    "BK": [{"BM": 1}, {"BN": 8}],
-    "BL": [],
-    "BM": [],
-    "BN": [{"BO": 2}, {"BP": 14}],
-    "BO": [],
-    "BP": [{"BQ": 2}, {"BR": 11}],
-    "BQ": [{"BY": 1}, {"BZ": 4}],
-    "BR": [{"BS": 5}, {"BT": 1}],
-    "BS": [],
-    "BT": [{"BU": 2}, {"BV": 5}],
-    "BU": [],
-    "BV": [{"BX": 2}, {"BW": 1}],
-    "BW": [],
-    "BX": [],
-    "BY": [],
-    "BZ": [{"CA": 3}, {"CB": 1}],
-    "CA": [{"CC": 3}, {"CD": 4}],
-    "CB": [],
-    "CC": [{"CE": 1}, {"CF": 1}],
-    "CD": [],
-    "CE": [{"CH": 1}, {"CG": 1}],
-    "CF": [],
-    "CG": [],
-    "CH": []
+    "A15": [{"B20": 8}, {"A11": 11}],
+    "A11": [{"C9": 30}, {"A12": 1}],
+    "B20": [{"G17": 14}, {"C20": 1}],
+    "G17": [{"J20": 8}, {"I18": 3}],
+    "C20": [{"C19": 1}, {"H20": 5}],
+    "C19": [],
+    "H20": [],
+    "J20": [{"J17": 5}, {"N20": 6}],
+    "I18": [],
+    "J17": [{"K15": 3}, {"L14": 11}],
+    "N20": [{"N18": 2}, {"Q20": 3}],
+    "N18": [],
+    "Q20": [],
+    "K15": [],
+    "L14": [{"R19": 15}, {"R15": 9}],
+    "R19": [{"S19": 1}, {"T16": 9}],
+    "R15": [{"P13": 6}, {"P16": 7}],
+    "P13": [{"Q13": 1}, {"N13": 2}],
+    "P16": [{"M16": 5}, {"P18": 2}],
+    "P18": [],
+    "M16": [],
+    "S19": [],
+    "T16": [{"T15": 1}, {"T13": 5}],
+    "T15": [],
+    "T13": [{"R8": 7}, {"P8": 9}],
+    "R8": [{"R7": 1}, {"R10": 4}],
+    "P8": [{"N10": 4}, {"T1": 37}],
+    "R7": [{"T17": 2}, {"Q7": 1}],
+    "R10": [],
+    "T17": [],
+    "Q7": [],
+    "N10": [{"N9": 1}, {"M10": 1}],
+    "T1": [],
+    "N9": [],
+    "M10": [],
+    "Q13": [],
+    "N13": [],
+    "C9": [{"B8": 2}, {"E10": 5}],
+    "A12": [],
+    "B8": [],
+    "E10": [{"D10": 1}, {"J10": 5}],
+    "D10": [],
+    "J10": [{"I12": 7}, {"H8": 4}],
+    "H8": [{"M6": 9}, {"G9": 2}],
+    "I12": [{"J12": 1}, {"H11": 2}],
+    "J12": [],
+    "H11": [{"I11": 1}, {"F12": 3}],
+    "I11": [],
+    "F12": [{"F11": 1}, {"F14": 2}],
+    "F11": [],
+    "F14": [{"G13": 4}, {"D15": 3}],
+    "G13": [],
+    "D15": [{"E14": 6}, {"B17": 4}],
+    "E14": [],
+    "B17": [],
+    "M6": [{"K8": 4}, {"P4": 5}],
+    "G9": [{"F8": 2}, {"H9": 1}],
+    "H9": [],
+    "F8": [],
+    "K8": [{"L8": 1}, {"J8": 1}],
+    "P4": [{"R4": 2}, {"P5": 1}],
+    "L8": [],
+    "J8": [],
+    "R4": [{"R1": 3}, {"S5": 2}],
+    "P5": [],
+    "R1": [{"S1": 1}, {"N1": 8}],
+    "S5": [],
+    "S1": [],
+    "N1": [{"P1": 2}, {"F3": 14}],
+    "P1": [],
+    "F3": [{"F1": 2}, {"J4": 11}],
+    "F1": [{"G1": 1}, {"D3": 4}],
+    "J4": [{"H5": 5}, {"K4": 1}],
+    "H5": [],
+    "K4": [{"K2": 2}, {"N4": 5}],
+    "K2": [],
+    "N4": [{"O3": 2}, {"M4": 1}],
+    "M4": [],
+    "O3": [],
+    "G1": [],
+    "D3": [{"A3": 3}, {"E3": 1}],
+    "A3": [{"B1": 3}, {"C5": 4}],
+    "E3": [],
+    "B1": [{"C1": 1}, {"A1": 1}],
+    "C5": [],
+    "C1": [{"D1": 1}, {"C2": 1}],
+    "A1": [],
+    "C2": [],
+    "D1": []
 }
 
 #create unvisitied queue
@@ -144,6 +146,9 @@ def dijkstra(graph, q):
                     sorted_q = sorted(q.items(), key=operator.itemgetter(1))
                     q = dict(sorted_q)
 
+                    #testing the straight line distance
+                    distance(x, end)
+
         #keep track of all the visited notes. Not essential but nice for being able to print stats
         visited.append(current)
 
@@ -166,6 +171,26 @@ def searchGraph(previous, visited, end):
 
     print("Shortest path to " + end + ": " + str(pathLen[end]))
     print("Nodes of shortest path from " + start + " to " + end + ": " + str(shortest))
+
+def distance(node1, node2):
+
+    #splits the string into column and row. Converts the letter to a column number
+    row1 = int(node1[1:])
+    col1 = string.ascii_lowercase.index(node1[0].lower()) + 1
+
+    row2 = int(node2[1:])
+    col2 = string.ascii_lowercase.index(node2[0].lower()) + 1
+
+    #calculated the difference between row values and column values
+    rowDif = abs(row1 - row2)
+    colDif = abs(col1 - col2)
+
+    #pythagorean formulae to calculate the distance
+    dist = sqrt(rowDif**2 + colDif**2)
+
+    print("Straight line distance: " + str(round(dist,1)))
+
+    return dist
 
 #run dijkstra
 previous, visited = dijkstra(graph, createQueue(graph, start))
